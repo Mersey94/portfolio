10efa3bd6a312963e23d2d8198607892e59ea3ae
Mersey <54132987+Mersey94@users.noreply.github.com>
2021-09-30 19:44:57 +1000

    updated homepage toggle / footer

--- src/script.js
+++ src/script.js
@@ -12,6 +12,7 @@ function myFunction() {
   /* Alert the copied text */
   alert("Copied the text: " + copyText.value);
 }
+
 $("#toggle").click(function () {
   $(this).toggleClass("on");
   $("#menu").slideToggle();
